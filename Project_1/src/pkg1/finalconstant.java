ae6227c09cc34634879039a8aedebc4dc5571877
fadin <fadin@Fadi>
2023-04-05 01:01:13 -0700

    fourth push

---
+++ Project_1/src/pkg1/finalconstant.java
@@ -0,0 +1,12 @@
+package pkg1;
+
+public final class finalconstant 
+{
+	final static int x=5;
+	public static void main(String[] args) 
+	{
+		System.out.println("the value of x is "+x);
+		
+	}
+
+}
